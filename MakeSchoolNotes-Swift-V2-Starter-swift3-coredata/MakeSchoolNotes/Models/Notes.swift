a1a83176922df674da0905c0056c7419fd9c146a
Shannon Ding <shannon_ding@yahoo.com>
2017-06-23 15:50:46 -0700

    added everything
    it was very stressful

---
+++ MakeSchoolNotes-Swift-V2-Starter-swift3-coredata/MakeSchoolNotes/Models/Notes.swift
@@ -0,0 +1,15 @@
+//
+//  Notes.swift
+//  MakeSchoolNotes
+//
+//  Created by Shannon Ding on 6/22/17.
+//  Copyright © 2017 MakeSchool. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    var title = ""
+    var content = ""
+    var modificationTime = Date()
+}
